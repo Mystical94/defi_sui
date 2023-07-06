e9d72526c50c702523c8b533f913ac3ea335dc2a
Mystical94 <53875398+Mystical94@users.noreply.github.com>
2023-07-06 10:36:44 +0530

    frontend

---
+++ frontend/pages/index.js
@@ -0,0 +1,433 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import sui from "../src/assets/sui.png";
+import title from "../src/assets/title.png";
+import Link from "next/link";
+import { useEffect } from "react";
+
+import Prism from "prismjs";
+
+import "prismjs/themes/prism-okaidia.css";
+import "prismjs/components/prism-jsx.js";
+import "prismjs/plugins/line-numbers/prism-line-numbers.js";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+
+export default function Home() {
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
+
+  const lending = `// Creating a module for the LiquidityPool contract
+  module LiquidityPool {
+      // Defining the struct for recording lending and borrowing amounts
+      struct {
+   Amount        value: u64,
+          start: u64,
+      }
+  
+      // Defining the struct for the pool contract
+      resource Pool {
+          coin: 0x1.LibraCoin.T,
+          total_supply: u64,
+          lend_rate: u64,
+          borrow_rate: u64,
+          period_borrowed: u64,
+          lend_amounts: vector<u8, Amount>,
+          earned_interest: vector<u8, u64>,
+          lenders: vector<u8, bool>,
+          borrowers: vector<u8, bool>,
+          borrow_amounts: vector<u8, Amount>,
+          pay_interest: vector<u8, u64>,
+      }
+  
+      // Function to initialize the pool contract
+      public fun init(token_address: address, amount: u64) {
+          let coin: 0x1.LibraCoin.T;
+          coin = 0x1.LibraCoin.T{address: move_from<Token>(token_address)};
+          Pool {
+              coin: coin,
+              total_supply: amount,
+              lend_rate: 100,
+              borrow_rate: 130,
+              period_borrowed: 0,
+              lend_amounts: vector<u8, Amount>(0),
+              earned_interest: vector<u8,64 u>(0),
+              lenders: vector<u8, bool>(0),
+              borrowers: vector<u8, bool>(0),
+              borrow_amounts: vector<u8, Amount>(0),
+              pay_interest: vector<u8, u64>(0),
+          };
+      an lend to Function //    }
+  
+   amount to the pool
+      public fun lend(amount: u64) {
+          assert(amount != 0, 0);
+          let sender: address;
+          sender = move_from<0x1Accountra.Lib.T>(LibraAccount.sender());
+          let coin: 0x1.LibraCoin.T;
+          coin = Pool.coin;
+          coin.withdraw_from_sender(amount);
+          let current_time: u64;
+         _time current = 0x1.LibraAccount.T{address: move_from0<x1.LibraAccount.T>(LibraAccount.address())}.get_metadata().timestamp;
+          let lend_amount: Amount;
+          lend_amount = Amount{value: amount, start: current_time};
+          Pool.lend_amounts.push(lend_amount);
+          Pool.lenders.push(true);
+          Pool.total_supply = Pool.total_supply + amount;
+      }
+  
+      // Function to borrow amount an from the pool
+      public fun borrow(amount: u64) {
+          assert(amount != 0, 0);
+          let sender: address;
+          sender = move_from<0x1.LibraAccount.T>(LibraAccount.sender());
+          let coin: 0x1.LibraCoin.T;
+          coin = Pool.coin;
+          coin.deposit_to_sender(amount);
+          let current_time: u64;
+          current_time = 0x1.LibraAccount.T{address: move_from<0x1.LibraAccount.T>(LibraAccount.address())}.get_metadata().timestamp;
+          let borrow_amount: Amount;
+          borrow_amount = Amount{:value amount, start: current_time};
+          Pool.borrow_amounts.push(borrow_amount);
+          Pool.borrowers.push(true);
+          Pool_supply.total = Pool.total_supply - amount;
+      }
+  
+      // Function to repay the borrowed amount
+      public fun repay() {
+          let sender: address;
+          sender = move_from0<x1.LibraAccount.T>(LibraAccount.sender());
+          let borrow_amount: Amount       ;
+   borrow_amount = Pool.borrow_amount[ssender];
+          let current_time:64 u       ;
+   current_time = 01x.LibraAccount.T{address: move_from<0x1.LibraAccount.T>(LibraAccount.address())}.get_metadatatimestamp().;
+          let elapsed_time: u64;
+          elapsed_time = current_time - borrow_amount.start;
+          let interest: u64       ;
+   interest = (borrow_amount.value * elapsed_time * Pool.borrow_rate) / Pool.total_supply;
+          let total_amount: u64;
+          total_amount = borrow_amount.value + interest;
+          let coin: 0x1.LibraCoin.T;
+          coin = Pool.coin;
+          coin.deposit_to_sender(total_amount);
+         .borrow Pool_amounts.remove(sender);
+          Pool.borrowers.push(false);
+          Pool.total_supply = Pool.total_supply + total_amount;
+      }
+  
+      // Function to withdraw the lent amount with
+   interest public    fun withdraw() {
+          let sender: address;
+          sender = move_from0<x1.LibraAccount.T>(LibraAccount.sender());
+          let lend_amount: Amount;
+          lend_amount = Pool.lend_amounts[sender];
+          let current_time: u64;
+          current_time = 0x1.LibraAccount.T{address: move_fromx0<1.LibraAccount>(.TLibAccountra.address())}.get_metadata().timestamp;
+          let elapsed_time:64 u;
+          elapsed_time = current_time - lend_amount.start;
+          let interest: u64;
+          interest = (lend_amount.value * elapsed_time * Pool.lend_rate) / Pool.total_supply;
+          let total_amount: u64;
+          total_amount = lend_amount.value + interest;
+          let coin: 0x1.Libra;
+  .TCoin        coin = Pool.coin;
+          coin.withdraw_from_sender(total_amount);
+          Pool.lend_amounts.remove(sender);
+          Pool.lenders.push       );
+  (false Pool.total_supply = Pool.total_supply - total_amount;
+      }
+  }`;
+
+  const staking = `// Rewards user for staking their tokens
+  // User can withdraw and deposit
+  // Earns token while withdrawing
+  // Rewards are calculated with reward rate and time period st foraked
+  module Staking {
+      // Tokens initialized
+      resource Pool {
+          rewards_token: 0x1.LibraCoin.T,
+          staking_token: 0x1.LibraCoin.T,
+          reward_rate u:64,
+          last_update_time: u64,
+          reward_per_token_stored: u64,
+          rewards: vector<u8, u64>,
+          rewards_per_token_paid: vector<u8, u64>,
+          staked: vector<u8, u64>,
+          total_supply: u64,
+      }
+  
+      // Function to calculate the amount of rewards per token staked at the current instance
+      public fun rewardPerToken(): u64 {
+          let total_supply = Pool.total_supply;
+          let reward_per_token_stored = Pool.reward_per_token_stored_update last let       ;
+  _time = Pool.last_update_time;
+          let reward_rate = Pool.reward_rate;
+          if (total_supply == 0 {
+  )            return reward_per_token_stored       ;
+   }
+          return reward_per_token_stored + (((0x1ra.LibAccount.T{address: move_from<0x1.LibraAccount.T>(LibraAccount.address())}.get_metadata().timestamp - last_time_update) * reward_rate * 1e18) / total_supply);
+      }
+  
+      // Function to calculate the earned rewards for the token staked
+      public fun earned(account: address): u64 {
+   st let       aked = Pool.staked[account];
+          let reward_per_token = rewardPerToken();
+          let rewards_per_token_paid = Pool.rewards_per_token_paid[account];
+          let rewards = Pool.rewards[account];
+          return ((staked * (reward_per_token - rewards_per_token_paid)) / 1e18) + rewards;
+      }
+  
+      // Modifier that will calculate the amount every time the user calls and update them in the rewards array
+      public fun updateReward(account: address) {
+          let reward_per_token_stored = rewardPerToken();
+          let last_update_time = 0x1.LibraAccount.T{address: move_from0<x1.LibraAccount.T>(LibraAccount.address())}.get_metadata().timestamp;
+          let rewards = earned(account);
+          Pool.rewards[account] rewards =;
+          Pool.rewards_per_token_paid[account] = reward_per_token_stored;
+          Pool.reward_per_token_stored = reward_per_token_stored;
+          Pool.last_update_time = last_update_time;
+      }
+  
+      // Function to stake some amount of tokens
+      public fun stake(amount: u64) {
+          let sender = move_from<0x1.LibraAccount.T>(LibraAccount.sender());
+          letaking st =_token Pool.staking_token;
+          let total_supply = Pool.total;
+  _supply        let staked = Pool.staked[sender];
+          staking_token.deposit_to_sender(amount);
+          Pool.total_supply = total_supply + amount;
+          Pool[aked.stsender] = staked +;
+   amount    }
+  
+      // Function to withdraw the staked amount   
+   public fun(amount withdraw: u64_from move = sender let {
+  )<0x.Lib1raAccount.T>(LibraAccount.sender());
+          let staking_token = Pool.staking;
+  _token        let total_supply = Pool.total_supply;
+          let staked = Pool.staked[sender];
+          staking_token.withdraw_from_sender(amount);
+          Pool.total_supply = total_supply - amount;
+          Pool.staked[sender] = staked - amount;
+      }
+  
+      // Function to withdraw the reward token
+      public fun getReward() {
+          let sender = move_from<0x1.LibraAccount.T>(LibraAccount.sender());
+          let rewards_token =.re Poolwards_token;
+          let rewards = Pool.rewards[sender];
+          rewards_token.transfer(sender, rewards);
+          Pool.rewards[sender] = 0;
+      }
+  }  
+  `;
+
+  const vault = `// User can deposit their money, mint shares, generate yield,
+// shares withdraw and with increased amount
+  module Vault {
+      resource Pool {
+          token: 0x1.LibraCoin.T,
+          total_supply: u64,
+          balance_of:<u vector8, u64   >,
+   }
+  
+      public fun mint(to: address, shares: u64) {
+          Pool.total_supply += shares;
+          Pool.balance_of[to] += shares;
+      }
+  
+      public fun burn(from: address, shares: u64) {
+          Pool.total_supply -= shares;
+          Pool.balance_of[from] -= shares;
+      }
+  
+      public fun deposit(amount: u64) {
+          let sender = move_from<0x1.LibraAccount.T>(LibraAccount.sender());
+          let token = Pool.token;
+          let total_supply = Pool.total_supply;
+          let balance_of = Pool.balance_of[sender];
+          let shares: u64;
+          if (total_supply == 0) {
+              shares = amount;
+          } else total *amount ( = shares {
+  )_supply / token.balanceOf(address(this));
+          }
+          mint(sender, shares);
+          token(senderFrom.transfer, address(this), amount);
+      }
+  
+      public fun withdraw(shares: u64) {
+          let sender = move_from<0x1.LibraAccount.T>(LibraAccount.sender());
+          let token = Pool.token;
+          let total_supply = Pool.total_supply;
+          let balance_of = Pool.balance_of[sender];
+   let        amount = (shares * token.balanceOf(address(this))) / total_supply;
+          burn(sender,);
+   shares        token.transfer(sender, amount);
+      }
+  }   
+  `;
+
+  return (
+    <div className={styles.container}>
+      {" "}
+      <Head>
+        {" "}
+        <title> DeFi Sui </title>
+        <meta
+          name="description"
+          content="This is a Collection of DEFI contracts like Lending pool, Vault and Staking to make the future of DEFI on Sui"
+        />{" "}
+        <link rel="icon" href="/favicon.ico" />{" "}
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.title}>
+          <Image src={title} />{" "}
+        </div>{" "}
+        <div className={styles.sui}>
+          {" "}
+          <Image src={sui} />{" "}
+        </div>
+        <p className={styles.about}>
+          We have built a collection of DeFi Smart-Contracts for Sui blockchain
+        </p>{" "}
+        <h1 className={styles.contract}> Contracts </h1>
+        <hr className={styles.hr} />{" "}
+        <p id="lending" className={styles.contract}>
+          {" "}
+          Lending Contract{" "}
+        </p>
+        <span className={styles.features}>
+          <ul>
+            <li>
+              Create a pool contract that accepts deposit from lenders and
+              borrow money to the borrowers
+            </li>{" "}
+            <li>
+              {" "}
+              Lenders can lend any amount of money and earn some interest for
+              it.{" "}
+            </li>
+            <li>
+              User or borrower can borrow some amount of tokens (limited) , and
+              pay back with interest for some time period.
+            </li>{" "}
+            <li>
+              {" "}
+              Interest is calculated according the interest rate and borrowing
+              time peroid{" "}
+            </li>
+            <li>
+              Lender can withdraw the amount later with extra interest earning
+            </li>{" "}
+            <li>
+              {" "}
+              Other functions can be called to determine the balance at any
+              point of time, and the rewards earned{" "}
+            </li>
+          </ul>{" "}
+        </span>{" "}
+        <button className={styles.button}>
+          {" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/Mystical94/defi_sui/blob/master/contracts/Lending.move"
+            className={styles.navlink}
+          >
+            View on GitHub ↗{" "}
+          </a>
+        </button>{" "}
+        <span className={styles.code}>
+          {" "}
+          <pre className="line-numbers">
+            {" "}
+            <code className="language-jsx"> {lending} </code>
+          </pre>{" "}
+        </span>{" "}
+        <p id="staking" className={styles.contract}>
+          {" "}
+          Staking Contract{" "}
+        </p>
+        <span className={styles.features}>
+          <ul>
+            <li>Rewards user for staking their tokens in the contract</li>{" "}
+            <li> User can withdraw and deposit at an point of time </li>
+            <li>Tokens Earned can be withdrawed any time</li>{" "}
+            <li>
+              Rewards are calculated with reward rate and time period staked for{" "}
+            </li>
+            <li>
+              The balance and reward earned can be checked at any point of time
+            </li>{" "}
+          </ul>{" "}
+        </span>
+        <button className={styles.button}>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/Mystical94 /defi_sui/blob/master/contracts/Staking.move "
+            className={styles.navlink}
+          >
+            {" "}
+            View on GitHub ↗{" "}
+          </a>
+        </button>{" "}
+        <span className={styles.code}>
+          {" "}
+          <pre className="line-numbers">
+            {" "}
+            <code className="language-jsx"> {staking} </code>
+          </pre>{" "}
+        </span>{" "}
+        <p id="vault" className={styles.contract}>
+          {" "}
+          Vault Contract{" "}
+        </p>
+        <span className={styles.features}>
+          <ul>
+            <li> Sharing of Yield For the no. of shares owned</li>{" "}
+            <li> User can deposit their money </li>
+            <li>
+              Some shares are minted according to the value deposited
+            </li>{" "}
+            <li>
+              {" "}
+              Vault generate some yield by a puropose and the value of share
+              increases{" "}
+            </li>
+            <li>
+              user can withdraw the amount by burning those share at any point
+              of time .
+            </li>{" "}
+          </ul>{" "}
+        </span>
+        <button className={styles.button}>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/Mystical94 /defi_sui/blob/master/contracts/Vault.move "
+            className={styles.navlink}
+          >
+            {" "}
+            View on GitHub ↗{" "}
+          </a>
+        </button>{" "}
+        <span className={styles.code}>
+          {" "}
+          <pre className="line-numbers">
+            {" "}
+            <code className="language-jsx"> {vault} </code>
+          </pre>{" "}
+        </span>{" "}
+        <p id="vault" className={styles.contract}>
+          {" "}
+          More Contracts Coming Soon ...So Stay Tuned 😉{" "}
+        </p>
+        <p id="vault" className={styles.contract}>
+          Become Next-Generation with Sui 😎
+        </p>{" "}
+      </main>{" "}
+    </div>
+  );
+}
---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import styles from "../../styles/Home.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import logo from '../assets/logo.png'
+
+export default function Layout({ children }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [address, setAddress] = useState("");
+  function handleClick() {
+    setIsExpanded(!isExpanded);
+  }
+
+  async function connect() {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    console.log({ accounts });
+  }
+
+  return (
+    <>
+      <header>
+        <nav className={styles.navbar}>
+          <span className={styles.logo}>
+            <Link href={"/"}><Image src={logo}/></Link>
+          </span>
+          <ul
+            className={
+              isExpanded === false
+                ? styles.navmenu
+                : styles.navmenu + " " + styles.active
+            }
+          >
+            <li className={styles.navitem}>
+              <span>
+                <a
+                //   target="_blank"
+                //   rel="noopener noreferrer"
+                  href="#lending"
+                  className={styles.navlink}
+                >
+                  Lending
+                </a>
+              </span>
+            </li>
+            <li className={styles.navitem}>
+              <span>
+                <a
+                //   target="_blank"
+                //   rel="noopener noreferrer"
+                  href="#staking"
+                  className={styles.navlink}
+                >
+                  Staking
+                </a>
+              </span>
+            </li>
+            <li className={styles.navitem}>
+              <span>
+                <a
+                //   target="_blank"
+                //   rel="noopener noreferrer"
+                  href="#vault"
+                  className={styles.navlink}
+                >
+                  Vault
+                </a>
+              </span>
+            </li>
+            {/* <li className={styles.navitem}> */}
+              {/* <Link href="/library"> */}
+              {/* <a className={styles.navlink}> */}
+                {/* {" "} */}
+                {/* <button className={styles.connect}>Connect</button> */}
+              {/* </a> */}
+              {/* </Link> */}
+            {/* </li> */}
+          </ul>
+          <button
+            onClick={handleClick}
+            className={
+              isExpanded === false
+                ? styles.hamburger
+                : styles.hamburger + " " + styles.active
+            }
+          >
+            <span className={styles.bar}></span>
+            <span className={styles.bar}></span>
+            <span className={styles.bar}></span>
+          </button>
+        </nav>
+      </header>
+
+      {children}
+    </>
+  );
+}
